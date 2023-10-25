44be572459f2d83c0799bdafc8294c39388fe138
stefanyccosta <stefanycristina97.sc@gmail.com>
2023-10-25 17:21:24 -0300

    Payment

--- src/main/java/com/devstefany/dscommerce/entities/Order.java
+++ src/main/java/com/devstefany/dscommerce/entities/Order.java
@@ -1,5 +1,6 @@
 package com.devstefany.dscommerce.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import java.time.Instant;
@@ -24,5 +26,57 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private  Payment payment;
 
+    public Order(){
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
