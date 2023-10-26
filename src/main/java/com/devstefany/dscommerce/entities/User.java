3f5c6891f80f93c6faa35fa36b254b806e8dae3c
stefanyccosta <stefanycristina97.sc@gmail.com>
2023-10-26 16:39:18 -0300

    creation of category and products classes.

--- src/main/java/com/devstefany/dscommerce/entities/User.java
+++ src/main/java/com/devstefany/dscommerce/entities/User.java
@@ -1,5 +1,6 @@
 package com.devstefany.dscommerce.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String phone;
     private LocalDate birthDate;
