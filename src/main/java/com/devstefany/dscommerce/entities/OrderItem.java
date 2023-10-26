cb56a7d530510ebe027ef0558dc18e50d0577cf1
stefanyccosta <stefanycristina97.sc@gmail.com>
2023-10-26 17:01:45 -0300

    orderItem class created

---
+++ src/main/java/com/devstefany/dscommerce/entities/OrderItem.java
@@ -0,0 +1,47 @@
+package com.devstefany.dscommerce.entities;
+
+public class OrderItem {
+    private OrderItemPK id = new OrderItemPK();
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(){
+
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder(){
+      return id.getOrder();
+    }
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+    public Product getProduct(){
+        return id.getProduct();
+    }
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+}
